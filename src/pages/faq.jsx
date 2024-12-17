c07f6911e701e679f8f6a8b81bc6744816193dac
Matones01 <matthewadamjones99@gmail.com>
2024-12-17 19:25:19 +0000

    FAQ page basic styling

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -5,41 +5,62 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Link} from 'react-router-dom';
-import { Typography } from "@mui/material";
+import { List, Typography } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import api from '../api/axios';
 
 function FAQS(){
     const [list, setList] = useState([])
 
     useEffect(() => {
-        fetch('https://final-project-frontend2.onrender.com/faq/').then(
-        (response) => response.json()
-    ).then(
-        (data) => setList(data)
-    )
-     }, [])
+    const getAllFAQ = async () => { 
+        const response = await api.get('FAQAPI/')
+        setList(response.data.data)
+ 
+ 
+        return list;
+    }
+    getAllFAQ()
+}, []);
+    
+
+    //useEffect(() => {
+    //    fetch('https://675d722063b05ed07977e472.mockapi.io/faq').then(
+    //    (response) => response.json()
+    //).then(
+    //    (data) => setList(data)
+    //)
+    // }, [])
 
     return (
         <>
-        <div>
-            <ul>
+        <div className="FAQall">
+            <div className="FAQ">
+            <h1>FAQ's</h1>
+            </div>
+        <div className="FAQlist">
+            <div className="PopQuest">
+            <h2>Popular Questions</h2>
+            </div>
+            <div>
+            <ul className="Questions">
                 {list.map((list) =>
                 <li>
-                    <Accordion>
+                    <Accordion className="Question">
                         <AccordionSummary expandIcon= {<ExpandMoreIcon/>} aria-controls="panel1a-content" id="panel1a-header">
-
-                            <Typography>
-                                {data.name}
+                            <Typography className="FAQ-question">
+                                {list.question}
                             </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        {data.content}
+                        {list.answer}
                     </AccordionDetails>
                     </Accordion>
                 </li>
             )}
             </ul>
+            </div>
+        </div>
         </div>
         </>
     )
