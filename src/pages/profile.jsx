82d2ce152f514d7ac84e12f6f9ae7e6ec89275a2
Matones01 <matthewadamjones99@gmail.com>
2024-12-19 18:57:42 +0000

    Profile page stuff

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,9 +1,24 @@
 //Profile page
-import React from "react";
-
+import React, {useContext} from "react";
+import { useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import profilePic from '../assets/SBC24-Final-project-blank-profile-pic.png';
+//change first name last name 
 function Profile(){
+    const { user } = useContext(UserContext)
     return (
-        <></>
+        <>
+            <div className="Profile">
+                <div className="profile-Picture">
+                    <img src={profilePic} alt="profile-picture"/>
+                </div>
+                <div className="profile-Options">
+                <div className="profile-Greeting">
+                    <h1>Hello {user.username}</h1>
+                </div>
+                </div>
+            </div>
+        </>
     )
 }
 
